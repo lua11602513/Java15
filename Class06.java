a56769a8def2bc25279fe25c136d8457d03ab8d5
lua11602513 <113486725+lua11602513@users.noreply.github.com>
2022-12-19 08:32:40 +0800

    Add files via upload

---
+++ Class06.java
@@ -0,0 +1,25 @@
+class Add implements Runnable{
+	private int n;
+	private int sum=0;
+	public Add(int a){
+		n=a;
+}
+	public void run(){
+		for(int i=1;i<=n;i++)
+{
+	for(int j=0;j<100000000;j++);
+	sum+=i;
+}
+	System.out.println("1+2+...+"+n+"="+sum);
+}
+}
+public class Class06{
+	public static void main(String args[]){
+		Add a1=new Add(5);
+		Add a2=new Add(10);
+		Thread t1=new Thread(a1);
+		Thread t2=new Thread(a2);
+		t1.start();
+		t2.start();
+}
+}
