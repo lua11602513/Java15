ff2a9c110ac7159ce9903c237f99f0e327d10d46
lua11602513 <113486725+lua11602513@users.noreply.github.com>
2022-12-19 08:41:44 +0800

    Add files via upload

---
+++ Class10.java
@@ -0,0 +1,31 @@
+class CTest implements Runnable{
+	private String id;
+	public CTest(String str){
+		id=str;
+}
+	public void run(){
+		for(int i=1;i<=5;i++){
+			try{
+				Thread.sleep((int)(2000*Math.random()));
+}
+	catch(InterruptedException e){}
+			System.out.println(id+" "+i);
+}
+}
+}
+public class Class10{
+	public static void main(String args[]){
+		CTest hi=new CTest("Hello");
+		CTest bye=new CTest("Good bye");
+		CTest morning=new CTest("Good morning");
+		CTest night=new CTest("Good night");
+		Thread t1=new Thread(hi);
+		Thread t2=new Thread(bye);
+		Thread t3=new Thread(morning);
+		Thread t4=new Thread(night);
+		t1.start();
+		t2.start();
+		t3.start();
+		t4.start();
+}
+ }	
